83f20a49bc60c5637b402b17003eade708b3e665
franfif <francis.barrow@gmail.com>
2023-03-06 14:57:44 -0500

    Add signup view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from authentication import forms
 
-# Create your views here.
+
+def signup_page(request):
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.post)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    else:
+        form = forms.SignUpForm()
+    return render(request,
+                  'authentication/signup.html',
+                  context={'form': form})
