2529c4d188e49edaf3956c3439d931e02e7c5975
franfif <francis.barrow@gmail.com>
2023-03-07 10:45:09 -0500

    add path to media in url patterns

--- litreview/urls.py
+++ litreview/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, \
     LogoutView, PasswordChangeView, PasswordChangeDoneView
@@ -26,3 +28,7 @@ urlpatterns = [
          reviews.views.create_ticket_and_review,
          name='create_ticket_review'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
