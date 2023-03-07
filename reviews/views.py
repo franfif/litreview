37d750090ead389e1f86534761f8524ee323466e
franfif <francis.barrow@gmail.com>
2023-03-07 10:41:22 -0500

    add create views for tickets and reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,49 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
+from . import forms, models
 
 @login_required()
 def home_page(request):
     hello_world = "Hellow World!"
     return render(request,
                   'reviews/home_page.html',
-                  context={'greetings': hello_world})
+                  context={'tickets_and_reviews': tickets_and_reviews})
+
+
+@login_required
+def create_ticket(request):
+    form = forms.TicketForm()
+    if request.method == 'POST':
+        form = forms.TicketForm(request.POST, request.FILES)
+        if form.is_valid():
+            ticket = form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+            return redirect('home')
+    return render(request,
+                  'reviews/create_ticket.html',
+                  context={'form': form})
+
+
+@login_required
+def write_review_from_ticket(request, ticket_id):
+    form = forms.ReviewForm()
+    ticket = get_object_or_404(models.Ticket, id=ticket_id)
+    if request.method == 'POST':
+        form = forms.ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.ticket = ticket
+            review.save()
+            return redirect('home')
+    return render(request,
+                  'reviews/write_review.html',
+                  context={'form': form,
+                           'ticket': ticket})
+
+
+@login_required
+def create_ticket_and_review(request):
+    pass
