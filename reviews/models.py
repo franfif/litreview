ca3621f53f2ab5465972bdb2f045ccc7056a735d
franfif <francis.barrow@gmail.com>
2023-03-07 10:59:08 -0500

    Update Ticket model to add resize image function on save

--- reviews/models.py
+++ reviews/models.py
@@ -1,15 +1,28 @@
+from PIL import Image
+
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class Ticket(models.Model):
+    IMAGE_MAX_SIZE = (700, 200)
+
     title = models.CharField(max_length=128)
     description = models.TextField(max_length=2048, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
+
+    def resize_image(self):
+        image = Image.open(self.image)
+        image.thumbnail(self.IMAGE_MAX_SIZE)
+        image.save(self.image.path)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image()
 
 
 class Review(models.Model):
