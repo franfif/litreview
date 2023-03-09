860fa68ff6e8f65552697357de07bb545547d0c1
franfif <francis.barrow@gmail.com>
2023-03-09 17:16:42 -0500

    add crispy helper form to Login and Signup forms

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,15 +1,35 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 
+FORM_CLASS = 'form-signin mx-auto'
+
+
 class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = FORM_CLASS
+
+        self.helper.add_input(Submit('submit', 'Log In'))
+
     username = forms.CharField(max_length=63)
     password = forms.CharField(max_length=63,
                                widget=forms.PasswordInput)
 
 
 class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = FORM_CLASS
+
+        self.helper.add_input(Submit('submit', 'Submit'))
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ['username']
