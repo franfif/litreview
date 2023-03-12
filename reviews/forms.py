376c45fea316decfbc6608063cde307d25e3f02e
franfif <francis.barrow@gmail.com>
2023-03-12 12:00:11 -0400

    add cripsy-forms and style to forms

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,15 +1,43 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, HTML, Div
+
 from django import forms
 
 from . import models
 
 
 class TicketForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.disable_csrf = True
+        self.helper.include_media = True
+        self.helper.layout = Layout(
+            Field('title'),
+            Field('description'),
+            Div(
+                HTML("""{% if ticket.image %}<img class="col-auto" 
+                     src="{{ ticket.image.url }}">{% endif %}"""),
+                Field('image', wrapper_class='col'),
+                css_class='row mb-3'
+            )
+        )
+
     class Meta:
         model = models.Ticket
         fields = ['title', 'description', 'image']
 
 
 class ReviewForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.disable_csrf = True
+        # self.helper.add_input(Submit('submit', 'Submit', css_class='float-right'))
+
     class Meta:
         model = models.Review
-        fields = ['rating', 'headline', 'body']
+        fields = ['headline', 'rating', 'body']
--- reviews/models.py
+++ reviews/models.py
@@ -35,7 +35,8 @@ class Ticket(models.Model):
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(5)])
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        choices=((0, '0'), (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')))
     headline = models.CharField(max_length=128)
     body = models.TextField(max_length=8192, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
